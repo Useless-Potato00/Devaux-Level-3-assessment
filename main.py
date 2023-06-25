3036870ba3e017534eb06b97dfd553126f2aa206
Useless-Potato00 <79672214+Useless-Potato00@users.noreply.github.com>
2023-06-25 21:03:40 +0000

    Working with Tkinter functions, (.Label, .Button, text, bg, fg)

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import tkinter as tk
+from tkinter import ttk
 
 window = tk.Tk()
-ro = tk.Label(text="Python Rocks!")
+window.geometry("400x300")
+
+
+ro = tk.Label(text="Python Rocks!", bg="Brown", fg="Cyan")
 ro.pack()
 
+ret = tk.Label(bg="Chocolate", fg="Chocolate", width="300", height="300")
+ret.pack()
+
+h=ttk.Button(text="Python")
+h.pack()
+
 window.mainloop()
-a
