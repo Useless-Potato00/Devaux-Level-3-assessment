d05b8af4d537eeb515dfd18e17cd1b761c5edab0
Useless-Potato00 <79672214+Useless-Potato00@users.noreply.github.com>
2023-09-11 23:40:39 +0000

    Added the ability to change username

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import tkinter as tk
+from tkinter import simpledialog
+from tkinter.simpledialog import askstring 
 class clas:
+    
     def __init__(self): #this is what runs when the class is called and makes the window and background
         self.window = tk.Tk()
         self.window.geometry("400x300")
         
-        self.background=tk.Frame(self.window, height=300, width=400, bg="green")
+        self.background =tk.Frame(self.window, height=300, width=400)
         self.background.place(relx=0.5, rely=0.5, anchor="center")
 
         self.definition() #this runs the second def in this class
@@ -12,20 +15,28 @@ class clas:
 
     def definition(self): # This is the second def and is only run when you call it. This sets the labels and buttons
         
-        self.window2 = tk.Label(self.window, text="Your first window!")
-        self.window2.pack()
+        self.neww = tk.Label(self.window, text="Your first window!")
+        self.neww.pack()
         
-        self.bt1=tk.Button(self.window, text="Create new window?", command=clas2)
+        self.bt1 = tk.Button(self.window, text="New", command=clas2) #New window button
         self.bt1.pack()
 
+        self.bt2 = tk.Button(self.window, text="set username", command=clas3)
+        self.bt2.pack()
+
 class clas2: 
-    def __init__(self):#When Clas2 is called it will run this definiton
+    def __init__(self): #When Clas2 is called it will run this definiton (Window 2)
         
         self.window = tk.Tk()
         self.window.geometry("400x300")
     
         self.window2 = tk.Label(self.window, text="Second Window")
         self.window2.pack()
-        
-        self.window.mainloop()
+
+class clas3:
+    def __init__(self):
+
+        self.yeet = askstring("Input name", "Input name")
+        lisit = [self.yeet]
+        print(lisit)
 clas()
